20e9c5a055c000575b2951787cc7770d7cc136e3
joselsm3 <joselsm3@al.insper.edu.br>
2026-02-25 16:19:53 -0300

    finalizado

--- main/main.c
+++ main/main.c
@@ -8,10 +8,61 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+const int BTN = 18;
+const int LED = 12;
+
+const int FASE_1 = 28;
+const int FASE_2 = 27;
+const int FASE_3 = 26;
+const int FASE_4 = 22;
+
+
 int main() {
     stdio_init_all();
+
+    gpio_init(BTN);
+    gpio_set_dir(BTN, GPIO_IN);
+    gpio_pull_up(BTN);
+
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+
+    gpio_init(FASE_1);
+    gpio_set_dir(FASE_1, GPIO_OUT);
+    
+    gpio_init(FASE_2);
+    gpio_set_dir(FASE_2, GPIO_OUT);
+    
+    gpio_init(FASE_3);
+    gpio_set_dir(FASE_3, GPIO_OUT);
+
+    gpio_init(FASE_4);
+    gpio_set_dir(FASE_4, GPIO_OUT);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        if (gpio_get(BTN) == 0) {
+            gpio_put(LED, 1);
+            sleep_ms(5);
+
+            for(int i = 0; i < 512; i++) {
+                gpio_put(FASE_1, 1);
+                sleep_ms(5);
+                gpio_put(FASE_1, 0);
+                gpio_put(FASE_2, 1);
+                sleep_ms(5);
+                gpio_put(FASE_2, 0);
+                gpio_put(FASE_3, 1);
+                sleep_ms(5);
+                gpio_put(FASE_3, 0);
+                gpio_put(FASE_4, 1);
+                sleep_ms(5);
+                gpio_put(FASE_4, 0);
+            }
+
+            gpio_put(LED, 0);
+            printf("Button pressed!\n");
+            sleep_ms(5);
+        }
+        
     }
 }
